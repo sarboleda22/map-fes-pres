4d42cbfead0ee2bab7110906db600280e7298908
Sebas <sebasarboleda22@gmail.com>
2018-03-29 15:16:46 -0400

    Changed map style

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2FyYm9sZWRhMjIiLCJhIjoiY2pkdWg3YXBzMWV0eDJ4bnlzeHk1bTA0ZCJ9.jFNUbOyVXti1PiWFL9LKag';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
+    style: 'mapbox://styles/sarboleda22/cjfcw8hjz1ey82smmo9llly5f',
     center: [-96.1476785, 40.2744218],
     maxZoom: 16,
     minZoom: 1,
