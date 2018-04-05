49aeadbea733339f5d8ced33df397fea72cc67d3
Seb <sebasarboleda22@gmail.com>
2018-04-05 12:02:25 -0400

    Changed saved (kWh) for savings

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ var locations = [{
     "id": "2",
     "title": "zColo 60 Hudson Data Center",
     "subtitle": "LED Lighting Upgrade",
-    "description": "At zColo’s landmark four floor data center at 60 Hudson, Fairbanks Energy completed a retrofit of their existing lighting with new efficient LED lighting with advanced controls. A majority of the fixtures now have wireless networking capabilities to allow for granular high and low trim light levels, occupancy and daylight harvesting capabilities. <br> <br> <br><br><b> TOTAL INCENTIVE: $28,000 <br> TOTAL ANNUAL kWh SAVED: 216,800 kWh <br> PROJECT PAYBACK: > 2 years </b>  <br> <br> <p style='font-size:19px;color:#337d49;'> <b>TOTAL ANNUAL SAVINGS: $40,000 </b></p>",
+    "description": "At zColo’s landmark four floor data center at 60 Hudson, Fairbanks Energy completed a retrofit of their existing lighting with new efficient LED lighting with advanced controls. A majority of the fixtures now have wireless networking capabilities to allow for granular high and low trim light levels, occupancy and daylight harvesting capabilities. <br> <br> <br><br><b> TOTAL INCENTIVE: $28,000 <br> TOTAL ANNUAL kWh SAVINGS: 216,800 kWh <br> PROJECT PAYBACK: > 2 years </b>  <br> <br> <p style='font-size:19px;color:#337d49;'> <b>TOTAL ANNUAL SAVINGS: $40,000 </b></p>",
     "picture": "img/zcolo1.jpg",
     "camera": {
         center: [-74.0104881, 40.7177228],
@@ -38,7 +38,7 @@ var locations = [{
     "id": "3",
     "title": "Vertex Pharmaceuticals",
     "subtitle": "LED Lighting Upgrade",
-    "description": "Fairbanks Energy has completed a series of energy efficiency initiatives at the corporate headquarters of Vertex Pharmaceuticals Incorporated. The efficiency project involved the engineering and implementation of numerous LED lighting and advanced controls retrofits through a variety of buildings on the Vertex campus, including mechanical rooms, labs, the lobby, and other public spaces.<br> <br> <b> TOTAL INCENTIVE: $800,000 <br> TOTAL ANNUAL kWh SAVED: 2,700,000 kWh <br> PROJECT PAYBACK: 2 years </b> <br> <br> <p style='font-size:19px;color:#337d49;'> <b> TOTAL ANNUAL SAVINGS: $435,000 </b> </p>",
+    "description": "Fairbanks Energy has completed a series of energy efficiency initiatives at the corporate headquarters of Vertex Pharmaceuticals Incorporated. The efficiency project involved the engineering and implementation of numerous LED lighting and advanced controls retrofits through a variety of buildings on the Vertex campus, including mechanical rooms, labs, the lobby, and other public spaces.<br> <br> <b> TOTAL INCENTIVE: $800,000 <br> TOTAL ANNUAL kWh SAVINGS: 2,700,000 kWh <br> PROJECT PAYBACK: 2 years </b> <br> <br> <p style='font-size:19px;color:#337d49;'> <b> TOTAL ANNUAL SAVINGS: $435,000 </b> </p>",
     "picture": "img/2.jpg",
     "camera": {
         center: [-71.0483887, 42.3534302],
@@ -49,7 +49,7 @@ var locations = [{
     "id": "1",
     "title": "111 8th Avenue Multi-Tenant Colocation Facility",
     "subtitle": "LED Lighting Upgrade",
-    "description": "At 111 8th Avenue’s main data center space in New York City, Fairbanks Energy retrofitted a tenant’s existing fluorescent fixtures to new LED drivers and lamps with occupancy sensors. In the mechanical and NOC spaces, Fairbanks Energy installed new LED fixtures with integrated occupancy and daylight sensors. As a result, annual electrical consumption at the data center has reduced annually by 136,000 kWh. <br> <br> <b>TOTAL INCENTIVE: $5,400 <br> TOTAL ANNUAL kWh SAVED: 136,000 kWh <br> PROJECT PAYBACK: 1.2 YEARS</b> <br><br> <p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS: $23,000</b></p>",
+    "description": "At 111 8th Avenue’s main data center space in New York City, Fairbanks Energy retrofitted a tenant’s existing fluorescent fixtures to new LED drivers and lamps with occupancy sensors. In the mechanical and NOC spaces, Fairbanks Energy installed new LED fixtures with integrated occupancy and daylight sensors. As a result, annual electrical consumption at the data center has reduced annually by 136,000 kWh. <br> <br> <b>TOTAL INCENTIVE: $5,400 <br> TOTAL ANNUAL kWh SAVINGS: 136,000 kWh <br> PROJECT PAYBACK: 1.2 YEARS</b> <br><br> <p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS: $23,000</b></p>",
     "picture": "img/3.jpg",
     "camera": {
         center: [-74.005576, 40.7414688],
@@ -115,7 +115,7 @@ var locations = [{
     "id": "14",
     "title": "Hotel 140",
     "subtitle": "LED Lighting Upgrades", 
-    "description": "Similarly, Fairbanks Energy completed a LED lighting and lighting controls upgrade throughout Hotel 140’s basement, elevator lobbies, hallways and kitchenette/lounge, saving the customer $19,000 annually. <br> <br> <b>TOTAL INCENTIVE $20,000 <br> TOTAL ANNUAL kWh SAVED: 117,000 kWh <br> PROJECT PAYBACK 3 YEARS <br><br></b> <br><br> <p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS: $19,000</b></p>",
+    "description": "Similarly, Fairbanks Energy completed a LED lighting and lighting controls upgrade throughout Hotel 140’s basement, elevator lobbies, hallways and kitchenette/lounge, saving the customer $19,000 annually. <br> <br> <b>TOTAL INCENTIVE $20,000 <br> TOTAL ANNUAL kWh SAVINGS: 117,000 kWh <br> PROJECT PAYBACK 3 YEARS <br><br></b> <br><br> <p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS: $19,000</b></p>",
     "picture": "img/7.jpg",
     "camera": {
         center: [-71.0766487, 42.3486305],
@@ -148,7 +148,7 @@ var locations = [{
     "id": "9",
     "title": "Providence College",
     "subtitle": "LED Lighting Upgrades", 
-    "description": "Fairbanks Energy completed an LED lighting and lighting controls upgrade at Providence College Phillips Memorial Library.  Fairbanks Energy retrofitted a total of 2,956 fixtures, saving Providence College $179,000 annually. <br> <br> <b> TOTAL INCENTIVE $477,000 <br> TOTAL ANNUAL kWh SAVED 2.2 million kWh <br> PROJECT PAYBACK 2 years </b> <br><br> <p style='font-size:19px;color:#337d49;'><b> TOTAL ANNUAL SAVINGS $179,000 </p>",
+    "description": "Fairbanks Energy completed an LED lighting and lighting controls upgrade at Providence College Phillips Memorial Library.  Fairbanks Energy retrofitted a total of 2,956 fixtures, saving Providence College $179,000 annually. <br> <br> <b> TOTAL INCENTIVE $477,000 <br> TOTAL ANNUAL kWh SAVINGS 2.2 million kWh <br> PROJECT PAYBACK 2 years </b> <br><br> <p style='font-size:19px;color:#337d49;'><b> TOTAL ANNUAL SAVINGS $179,000 </p>",
     "picture": "img/9.jpg",
     "camera": {
         center: [-71.4401479, 41.8440829],
@@ -170,7 +170,7 @@ var locations = [{
     "id": "10",
     "title": "zColo Hillcrest Data Center",
     "subtitle": "Data Center Airflow Management Project", 
-    "description": "At zColo’s 14,000 square feet Dallas data center, Fairbanks Energy implemented an airflow management upgrade providing adequate cooling to the racks of the equipment while allowing for units to be turned off or in hot stand-by. The project saves zColo $68,000 each year in electricity costs. <br> <br> <b> TOTAL ANNUAL kWh SAVED: 970,000 kWh <br> PROJECT PAYBACK: 1.4 years </b> <br><br> <p style='font-size:19px;color:#337d49;'> <b> TOTAL ANNUAL SAVINGS: $68,000 </b> </p>",
+    "description": "At zColo’s 14,000 square feet Dallas data center, Fairbanks Energy implemented an airflow management upgrade providing adequate cooling to the racks of the equipment while allowing for units to be turned off or in hot stand-by. The project saves zColo $68,000 each year in electricity costs. <br> <br> <b> TOTAL ANNUAL kWh SAVINGS: 970,000 kWh <br> PROJECT PAYBACK: 1.4 years </b> <br><br> <p style='font-size:19px;color:#337d49;'> <b> TOTAL ANNUAL SAVINGS: $68,000 </b> </p>",
     "picture": "img/10.jpeg",
     "camera": {
         center: [-96.7935828, 32.9230277],
