d8bbca242e6b37f22fb29aa571dd5566ecd5618c
Seb <sebasarboleda22@gmail.com>
2018-04-05 12:41:51 -0400

    Added markers for locations

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ var locations = [{
     "picture": "img/logo.png",
     "camera": {
         center: [-96.1476785, 40.2744218],
-        zoom: 4.35,
+        zoom: 4.15,
         bearing: 0,
         pitch: 0
     }
@@ -27,7 +27,7 @@ var locations = [{
     "id": "2",
     "title": "zColo 60 Hudson Data Center",
     "subtitle": "LED Lighting Upgrade",
-    "description": "At zColo’s landmark four floor data center at 60 Hudson, Fairbanks Energy completed a retrofit of their existing lighting with new efficient LED lighting with advanced controls. A majority of the fixtures now have wireless networking capabilities to allow for granular high and low trim light levels, occupancy and daylight harvesting capabilities. <br> <br> <br><br><b> TOTAL INCENTIVE: $28,000 <br> TOTAL ANNUAL kWh SAVINGS: 216,800 kWh <br> PROJECT PAYBACK: > 2 years </b>  <br> <br> <p style='font-size:19px;color:#337d49;'> <b>TOTAL ANNUAL SAVINGS: $40,000 </b></p>",
+    "description": "At zColo’s landmark four floor data center at 60 Hudson, Fairbanks Energy completed a retrofit of their existing lighting with new efficient LED lighting with advanced controls. A majority of the fixtures now have wireless networking capabilities to allow for granular high and low trim light levels, occupancy and daylight harvesting capabilities. <br><br><b> TOTAL INCENTIVE: $28,000 <br> <br> TOTAL ANNUAL kWh SAVINGS: 216,800 kWh <br><br> PROJECT PAYBACK: > 2 years </b>  <br> <br> <p style='font-size:19px;color:#337d49;'> <b>TOTAL ANNUAL SAVINGS: $40,000 </b></p>",
     "picture": "img/zcolo1.jpg",
     "camera": {
         center: [-74.0104881, 40.7177228],
@@ -38,7 +38,7 @@ var locations = [{
     "id": "3",
     "title": "Vertex Pharmaceuticals",
     "subtitle": "LED Lighting Upgrade",
-    "description": "Fairbanks Energy has completed a series of energy efficiency initiatives at the corporate headquarters of Vertex Pharmaceuticals Incorporated. The efficiency project involved the engineering and implementation of numerous LED lighting and advanced controls retrofits through a variety of buildings on the Vertex campus, including mechanical rooms, labs, the lobby, and other public spaces.<br> <br> <b> TOTAL INCENTIVE: $800,000 <br> TOTAL ANNUAL kWh SAVINGS: 2,700,000 kWh <br> PROJECT PAYBACK: 2 years </b> <br> <br> <p style='font-size:19px;color:#337d49;'> <b> TOTAL ANNUAL SAVINGS: $435,000 </b> </p>",
+    "description": "Fairbanks Energy has completed a series of energy efficiency initiatives at the corporate headquarters of Vertex Pharmaceuticals Incorporated. The efficiency project involved the engineering and implementation of numerous LED lighting and advanced controls retrofits through a variety of buildings on the Vertex campus, including mechanical rooms, labs, the lobby, and other public spaces.<br> <br> <b> TOTAL INCENTIVE: $800,000 <br><br> TOTAL ANNUAL kWh SAVINGS: 2,700,000 kWh <br><br> PROJECT PAYBACK: 2 years </b> <br> <br> <p style='font-size:19px;color:#337d49;'> <b> TOTAL ANNUAL SAVINGS: $435,000 </b> </p>",
     "picture": "img/2.jpg",
     "camera": {
         center: [-71.0483887, 42.3534302],
@@ -49,7 +49,7 @@ var locations = [{
     "id": "1",
     "title": "111 8th Avenue Multi-Tenant Colocation Facility",
     "subtitle": "LED Lighting Upgrade",
-    "description": "At 111 8th Avenue’s main data center space in New York City, Fairbanks Energy retrofitted a tenant’s existing fluorescent fixtures to new LED drivers and lamps with occupancy sensors. In the mechanical and NOC spaces, Fairbanks Energy installed new LED fixtures with integrated occupancy and daylight sensors. As a result, annual electrical consumption at the data center has reduced annually by 136,000 kWh. <br> <br> <b>TOTAL INCENTIVE: $5,400 <br> TOTAL ANNUAL kWh SAVINGS: 136,000 kWh <br> PROJECT PAYBACK: 1.2 YEARS</b> <br><br> <p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS: $23,000</b></p>",
+    "description": "At 111 8th Avenue’s main data center space in New York City, Fairbanks Energy retrofitted a tenant’s existing fluorescent fixtures to new LED drivers and lamps with occupancy sensors. In the mechanical and NOC spaces, Fairbanks Energy installed new LED fixtures with integrated occupancy and daylight sensors. As a result, annual electrical consumption at the data center has reduced annually by 136,000 kWh. <br> <br> <b>TOTAL INCENTIVE: $5,400 <br><br> TOTAL ANNUAL kWh SAVINGS: 136,000 kWh <br><br> PROJECT PAYBACK: 1.2 YEARS</b> <br><br> <p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS: $23,000</b></p>",
     "picture": "img/3.jpg",
     "camera": {
         center: [-74.005576, 40.7414688],
@@ -60,7 +60,7 @@ var locations = [{
     "id": "5",
     "title": "Massachusetts General Hospital",
     "subtitle": "Comprehensive Upgrades", 
-    "description": "Since 2014, Fairbanks Energy has contracted with Massachusetts General Hospital under their Strategic Energy Master Plan. The energy plan mandates energy conservation projects that improve the conditions of their patients, staff and the environment. Fairbanks Energy has completed more than 30 energy efficiency projects at MGH at a variety of the hospital’s buildings on its main campus, in addition to other efficiency enhancements in its facilities throughout Boston. <br><br> <b>TOTAL INCENTIVE: $1,600,000 <br> TOTAL ANNUAL kWh SAVINGS: 7,000,000 kWh </b> <br><br> <p style='font-size:18px;color:#337d49;'><b> TOTAL ANNUAL SAVINGS: $1,250,000</b></p> ",
+    "description": "Since 2014, Fairbanks Energy has contracted with Massachusetts General Hospital under their Strategic Energy Master Plan. The energy plan mandates energy conservation projects that improve the conditions of their patients, staff and the environment. Fairbanks Energy has completed more than 30 energy efficiency projects at MGH at a variety of the hospital’s buildings on its main campus, in addition to other efficiency enhancements in its facilities throughout Boston. <br><br> <b>TOTAL INCENTIVE: $1,600,000 <br><br> TOTAL ANNUAL kWh SAVINGS: 7,000,000 kWh </b> <br><br> <p style='font-size:18px;color:#337d49;'><b> TOTAL ANNUAL SAVINGS: $1,250,000</b></p> ",
     "picture": "img/5.jpg",
     "camera": {
         center: [-71.0710221, 42.3631542],
@@ -82,7 +82,7 @@ var locations = [{
     "id": "4",
     "title": "Edward-Elmhurst Health",
     "subtitle": "LED Lighting Upgrade",
-    "description": "Fairbanks Energy Services completed a series of energy efficiency upgrades at Edward-Elmhurst Health, resulting in total annual savings of $1.9 million. A turn-key solution, the project replaced or retrofitted more than 38,000 inefficient halogen, incandescent, fluorescent and metal halide fixtures with modern LEDs. A variety of indoor spaces were improved through the initiative, including: common areas, nurse stations, cafeterias, offices, mechanical rooms, conference rooms, fitness centers and parking areas. <br> <br> <b> TOTAL INCENTIVE: $343,000 <br> TOTAL ANNUAL kWh SAVINGS: 15,000,000 kWh <br> PROJECT PAYBACK: 2.6 Years </b> <br><br> <p style='font-size:17px;color:#337d49;'><b> TOTAL ANNUAL SAVINGS: $2,000,000</b></p>",
+    "description": "Fairbanks Energy Services completed a series of energy efficiency upgrades at Edward-Elmhurst Health, resulting in total annual savings of $1.9 million. A turn-key solution, the project replaced or retrofitted more than 38,000 inefficient halogen, incandescent, fluorescent and metal halide fixtures with modern LEDs. A variety of indoor spaces were improved through the initiative, including: common areas, nurse stations, cafeterias, offices, mechanical rooms, conference rooms, fitness centers and parking areas. <br> <br> <b> TOTAL INCENTIVE: $343,000 <br><br> TOTAL ANNUAL kWh SAVINGS: 15,000,000 kWh <br><br> PROJECT PAYBACK: 2.6 Years </b> <br><br> <p style='font-size:17px;color:#337d49;'><b> TOTAL ANNUAL SAVINGS: $2,000,000</b></p>",
     "picture": "img/4.jpg",
     "camera": {
         center: [-88.1216413, 41.8030472],
@@ -104,7 +104,7 @@ var locations = [{
     "id": "7",
     "title": "Hotel 140",
     "subtitle": "Steam System Upgrades", 
-    "description": "Fairbanks Energy Services completed a steam system upgrade at Hotel 140 consisting of several improvements including valve replacements and a wholesale replacement of all steam traps with more efficient venture nozzle units. The net result was savings of over $20,000 per year, resulting in a less than 2.5 year payback. <br> <br> <b> TOTAL PROJECT COST: $50,000 <br> PROJECT PAYBACK: 2.5 YEARS</b> <br><br> <p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS $20,000</b></p>",
+    "description": "Fairbanks Energy Services completed a steam system upgrade at Hotel 140 consisting of several improvements including valve replacements and a wholesale replacement of all steam traps with more efficient venture nozzle units. The net result was savings of over $20,000 per year, resulting in a less than 2.5 year payback. <br> <br> <b> TOTAL PROJECT COST: $50,000 <br><br> PROJECT PAYBACK: 2.5 YEARS</b> <br><br> <p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS $20,000</b></p>",
     "picture": "img/14.jpg",
     "camera": {
         center: [-71.0766487, 42.3486305],
@@ -115,7 +115,7 @@ var locations = [{
     "id": "14",
     "title": "Hotel 140",
     "subtitle": "LED Lighting Upgrades", 
-    "description": "Similarly, Fairbanks Energy completed a LED lighting and lighting controls upgrade throughout Hotel 140’s basement, elevator lobbies, hallways and kitchenette/lounge, saving the customer $19,000 annually. <br> <br> <b>TOTAL INCENTIVE $20,000 <br> TOTAL ANNUAL kWh SAVINGS: 117,000 kWh <br> PROJECT PAYBACK 3 YEARS <br><br></b> <br><br> <p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS: $19,000</b></p>",
+    "description": "Similarly, Fairbanks Energy completed a LED lighting and lighting controls upgrade throughout Hotel 140’s basement, elevator lobbies, hallways and kitchenette/lounge, saving the customer $19,000 annually. <br> <br> <b>TOTAL INCENTIVE $20,000 <br><br> TOTAL ANNUAL kWh SAVINGS: 117,000 kWh <br><br> PROJECT PAYBACK 3 YEARS <br><br></b> <br><br> <p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS: $19,000</b></p>",
     "picture": "img/7.jpg",
     "camera": {
         center: [-71.0766487, 42.3486305],
@@ -137,7 +137,7 @@ var locations = [{
     "id": "8",
     "title": "Trinity Health of New England",
     "subtitle": "Mechanical Upgrades", 
-    "description": "Trinity Health of New England contracted with Fairbanks Energy Services to develop and implement different mechanical efficiency solutions at three of its hospitals - St Francis Hospital and Mt Sinai Hospital in Hartford CT and Johnson Memorial Medical Center in Stafford Springs CT. The total cost of the efficiency solutions is approximately $6 million with annual gas and oil savings of over 260,000 therms. FES accessed the Connecticut Energy Efficiency Fund through Eversource and CNG for over $3,250,000 of incentives. <br>  <br> <b>TOTAL INCENTIVE: $2,000,000 <br> TOTAL ANNUAL kWh SAVINGS: 5,700,000 kWh </b><br><br><p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS: $980,000</b></p>",
+    "description": "Trinity Health of New England contracted with Fairbanks Energy Services to develop and implement different mechanical efficiency solutions at three of its hospitals - St Francis Hospital and Mt Sinai Hospital in Hartford CT and Johnson Memorial Medical Center in Stafford Springs CT. The total cost of the efficiency solutions is approximately $6 million with annual gas and oil savings of over 260,000 therms. FES accessed the Connecticut Energy Efficiency Fund through Eversource and CNG for over $3,250,000 of incentives. <br><br> <b>TOTAL INCENTIVE: $2,000,000 <br><br> TOTAL ANNUAL kWh SAVINGS: 5,700,000 kWh </b><br><br><p style='font-size:19px;color:#337d49;'><b>TOTAL ANNUAL SAVINGS: $980,000</b></p>",
     "picture": "img/8.jpg",
     "camera": {
         center: [-72.7006005, 41.7749634],
@@ -148,7 +148,7 @@ var locations = [{
     "id": "9",
     "title": "Providence College",
     "subtitle": "LED Lighting Upgrades", 
-    "description": "Fairbanks Energy completed an LED lighting and lighting controls upgrade at Providence College Phillips Memorial Library.  Fairbanks Energy retrofitted a total of 2,956 fixtures, saving Providence College $179,000 annually. <br> <br> <b> TOTAL INCENTIVE $477,000 <br> TOTAL ANNUAL kWh SAVINGS 2.2 million kWh <br> PROJECT PAYBACK 2 years </b> <br><br> <p style='font-size:19px;color:#337d49;'><b> TOTAL ANNUAL SAVINGS $179,000 </p>",
+    "description": "Fairbanks Energy completed an LED lighting and lighting controls upgrade at Providence College Phillips Memorial Library.  Fairbanks Energy retrofitted a total of 2,956 fixtures, saving Providence College $179,000 annually. <br> <br> <b> TOTAL INCENTIVE $477,000 <br><br> TOTAL ANNUAL kWh SAVINGS 2.2 million kWh <br><br> PROJECT PAYBACK 2 years </b> <br><br> <p style='font-size:19px;color:#337d49;'><b> TOTAL ANNUAL SAVINGS $179,000 </p>",
     "picture": "img/9.jpg",
     "camera": {
         center: [-71.4401479, 41.8440829],
@@ -159,7 +159,7 @@ var locations = [{
     "id": "13",
     "title": "Trinity Health of New England",
     "subtitle": "LED Lighting Upgrades", 
-    "description": "Fairbanks Energy Services also completed lighting and lighting controls efficiency solutions at three of Trinity Health of NEw England hospitals, with annual electricity savings of over 8 million kWh.<br>  <br> <b> LED Lighting Upgrades <br> TOTAL INCENTIVE: $2,900,000 <br> TOTAL ANNUAL kWh SAVINGS: 8,000,000 kWh</b><br><br><p style='font-size:19px;color:#337d49'><b>TOTAL ANNUAL SAVINGS: $1,400,000</b></p>",
+    "description": "Fairbanks Energy Services also completed lighting and lighting controls efficiency solutions at three of Trinity Health of NEw England hospitals, with annual electricity savings of over 8 million kWh.<br><br> TOTAL INCENTIVE: $2,900,000 <br><br> TOTAL ANNUAL kWh SAVINGS: 8,000,000 kWh</b><br><br><p style='font-size:19px;color:#337d49'><b>TOTAL ANNUAL SAVINGS: $1,400,000</b></p>",
     "picture": "img/13.jpg",
     "camera": {
         center: [-72.7006005, 41.7749634],
@@ -170,7 +170,7 @@ var locations = [{
     "id": "10",
     "title": "zColo Hillcrest Data Center",
     "subtitle": "Data Center Airflow Management Project", 
-    "description": "At zColo’s 14,000 square feet Dallas data center, Fairbanks Energy implemented an airflow management upgrade providing adequate cooling to the racks of the equipment while allowing for units to be turned off or in hot stand-by. The project saves zColo $68,000 each year in electricity costs. <br> <br> <b> TOTAL ANNUAL kWh SAVINGS: 970,000 kWh <br> PROJECT PAYBACK: 1.4 years </b> <br><br> <p style='font-size:19px;color:#337d49;'> <b> TOTAL ANNUAL SAVINGS: $68,000 </b> </p>",
+    "description": "At zColo’s 14,000 square feet Dallas data center, Fairbanks Energy implemented an airflow management upgrade providing adequate cooling to the racks of the equipment while allowing for units to be turned off or in hot stand-by. The project saves zColo $68,000 each year in electricity costs. <br> <br> <b> TOTAL ANNUAL kWh SAVINGS: 970,000 kWh <br><br> PROJECT PAYBACK: 1.4 years </b> <br><br> <p style='font-size:19px;color:#337d49;'> <b> TOTAL ANNUAL SAVINGS: $68,000 </b> </p>",
     "picture": "img/10.jpeg",
     "camera": {
         center: [-96.7935828, 32.9230277],
@@ -238,4 +238,78 @@ map.on('load', function() {
 
     // Start the playback animation for each borough
     playback(0);
+});
+
+
+// Markers of the FES Locations
+var geojson = {
+  type: 'FeatureCollection',
+  features: [{
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-70.9063307, 42.179856]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'MA'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-72.6770809, 41.7663459]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'CT'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-73.9757727, 40.7509091]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'NYC'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-88.0949075, 42.0606854]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'IL'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-105.0011815, 39.7499842]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'CO'
+    }
+  },
+]};
+
+// add markers to map
+geojson.features.forEach(function(marker) {
+
+  // create a HTML element for each feature
+  var el = document.createElement('div');
+  el.className = 'marker';
+
+  // make a marker for each feature and add to the map
+  new mapboxgl.Marker(el)
+  .setLngLat(marker.geometry.coordinates)
+  .addTo(map);
 });
